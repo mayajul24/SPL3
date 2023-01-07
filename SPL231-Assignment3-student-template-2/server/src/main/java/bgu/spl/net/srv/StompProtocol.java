647e40c476d38f512405f6856a77eea96efdd912
mayajul24 <mayajul@post.bgu.ac.il>
2023-01-07 13:08:24 +0200

    bjbj

--- SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/srv/StompProtocol.java
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/srv/StompProtocol.java
@@ -53,7 +53,26 @@ public class StompProtocol implements MessagingProtocol<String> {
     }
     public Frame createFrame(String command,HashMap<String,String> headers,String body)
     {
-        
+        if(command.equals("CONNECT"))
+        {
+            return new ConnectFrame(command, headers, body);
+        }
+        if(command.equals("DISCONNECT"))
+        {
+            return new DisconnectFrame(command, headers, body);
+        }
+        if(command.equals("SUBSCRIBE"))
+        {
+            return new SubscribeFrame(command, headers, body);
+        }
+        if(command.equals("UNSUBSCRIBE"))
+        {
+            return new UnsubscribeFrame(command, headers, body);
+        }
+        if(command.equals("SEND"))
+        {
+            return new SendFrame(command, headers, body);
+        }
     }
 
 }
