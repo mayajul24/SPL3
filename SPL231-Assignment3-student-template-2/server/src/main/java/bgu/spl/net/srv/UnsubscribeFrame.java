ab84e85605d864f1b8de3081595987f11caaf959
Noa Barak <banoa@post.bgu.ac.il>
2023-01-07 13:10:27 +0200

    123

---
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/srv/UnsubscribeFrame.java
@@ -0,0 +1,33 @@
+package bgu.spl.net.srv;
+
+import java.util.HashMap;
+
+public class UnsubscribeFrame extends Frame {
+    private String command;
+    private HashMap<String, String> headers;
+    private String body;
+
+    public UnsubscribeFrame(String command,HashMap<String,String> headers,String body)
+    {
+        this.command = command;
+        this.headers = headers;
+        this.body = body;
+    }
+    public String handleFrame()
+    {
+
+    }
+    public boolean isValid()
+    {
+
+    }
+    public String createError()
+    {
+
+    }
+    public String createReplayFrame()
+    {
+
+    }
+    
+}
