841c9493a80b41507a807b756316417fcf4b45d0
mayajul24 <mayajul@post.bgu.ac.il>
2023-01-07 15:11:47 +0200

    njnjn

--- SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/srv/User.java
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/srv/User.java
@@ -1,18 +1,20 @@
 package bgu.spl.net.srv;
 
+import java.util.LinkedList;
+
 public class User {
     private String userName;
     private String password;
     private int connectionId;
     private int id;
-    private List<Topics> topics;
+    private LinkedList<Topic> topics;
 
     public User(String userName, String password, int connectionId, int id){
         userName = userName;
         password = password;
         connectionId = connectionId;
         id = id;
-        topics = new LinkedList<topics>;
+        topics = new LinkedList<Topic>();
     }
 
 }
