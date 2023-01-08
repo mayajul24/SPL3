118e3c4a37adb70ee2aeb8be5452bd22252488dd
Noa Barak <banoa@post.bgu.ac.il>
2023-01-08 22:22:23 +0200

    axaxa

--- SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
@@ -1,7 +1,7 @@
 package bgu.spl.net.srv;
 
 import bgu.spl.net.api.MessageEncoderDecoder;
-import bgu.spl.net.api.MessagingProtocol;
+import bgu.spl.net.api.StompMessagingProtocol;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
@@ -9,14 +9,14 @@ import java.net.Socket;
 
 public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler<T> {
 
-    private final MessagingProtocol<T> protocol;
+    private final StompMessagingProtocol<T> protocol;
     private final MessageEncoderDecoder<T> encdec;
     private final Socket sock;
     private BufferedInputStream in;
     private BufferedOutputStream out;
     private volatile boolean connected = true;
 
-    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, MessagingProtocol<T> protocol) {
+    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, StompMessagingProtocol<T> protocol) {
         this.sock = sock;
         this.encdec = reader;
         this.protocol = protocol;
@@ -42,7 +42,7 @@ public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler
         }
 
     }
-    public MessagingProtocol<T> getProtocol()
+    public StompMessagingProtocol<T> getProtocol()
     {
         return protocol;
     }
