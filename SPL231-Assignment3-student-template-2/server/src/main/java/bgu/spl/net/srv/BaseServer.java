ac673cd7004b86780d906a5ddfd9e58365e812ea
Noa Barak <banoa@post.bgu.ac.il>
2023-01-08 22:29:26 +0200

    xsxsx

--- SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/srv/BaseServer.java
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/srv/BaseServer.java
@@ -12,13 +12,13 @@ import java.util.function.Supplier;
 public abstract class BaseServer<T> implements Server<T> {
 
     private final int port;
-    private final Supplier<MessagingProtocol<T>> protocolFactory;
+    private final Supplier<StompMessagingProtocol<T>> protocolFactory;
     private final Supplier<MessageEncoderDecoder<T>> encdecFactory;
     private ServerSocket sock;
 
     public BaseServer(
             int port,
-            Supplier<MessagingProtocol<T>> protocolFactory,
+            Supplier<StompMessagingProtocol<T>> protocolFactory,
             Supplier<MessageEncoderDecoder<T>> encdecFactory) {
 
         this.port = port;
