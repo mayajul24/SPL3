2c6550bc6d2bc69d0c609e50d0271106040730b3
mayajul24 <mayajul@post.bgu.ac.il>
2023-01-08 22:55:38 +0200

    vhbh

--- SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,26 +1,46 @@
 package bgu.spl.net.impl.stomp;
 
+import java.io.IOException;
+
 import bgu.spl.net.api.MessageEncoderDecoder;
 import bgu.spl.net.srv.Connections;
 import bgu.spl.net.srv.Server;
 import bgu.spl.net.srv.StompEncDec;
 import bgu.spl.net.srv.StompProtocol;
 
-public class StompServer implements Server<T> {
+public class StompServer<T> implements Server<T> {
 
     public static void main(String[] args) {
         // TODO: implement this
-        Server.threadPerClient(
+        
+        if(args[1] == "TPC")
+        {
+                Server.threadPerClient(
                 7777, //port
                 () -> new StompProtocol(), //protocol factory
                 StompEncDec::new //message encoder decoder factory
         ).serve();
-
-         Server.reactor(
+        }
+        else {
+                Server.reactor(
                  Runtime.getRuntime().availableProcessors(),
                  7777, //port
                  () -> new StompProtocol(), //protocol factory
                  StompEncDec::new //message encoder decoder factory
          ).serve();
+        }
+         
     }
+
+@Override
+public void close() throws IOException {
+        // TODO Auto-generated method stub
+        
+}
+
+@Override
+public void serve() {
+        // TODO Auto-generated method stub
+        
+}
 }
